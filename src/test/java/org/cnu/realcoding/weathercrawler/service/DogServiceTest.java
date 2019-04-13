2fea7293b5b4218cd08cc0afea29d5e8c1cd2111
Ryan Cheong <rcheong@riotgames.com>
2019-04-13 16:58:36 +0900

    Add before annotaion

--- src/test/java/org/cnu/realcoding/weathercrawler/service/DogServiceTest.java
+++ src/test/java/org/cnu/realcoding/weathercrawler/service/DogServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 
@@ -16,75 +17,63 @@ public class DogServiceTest {
     @Before
     public void setUp(){
         dogService = new DogService();
-    }
 
-    @Test
-    public void testInsertDog(){
         Dog dog = new Dog();
         dog.setName("허숙히");
         dog.setKind("시베리안 허스키");
-
         dogService.insertDog(dog);
 
-        List<Dog> dogs = dogService.getAllDogs();
-        System.out.println(dogs);
-        assertTrue(dogs.size() == 1);
-        assertFalse(dogs.size() == 0);
+        Dog dog2 = new Dog();
+        dog2.setName("멍뭉이");
+        dog2.setKind("포메리안");
+        dogService.insertDog(dog2);
+
+        Dog dog3 = new Dog();
+        dog3.setName("알콩이");
+        dog3.setKind("말티즈");
+        dogService.insertDog(dog3);
+
+        Dog dog4 = new Dog();
+        dog4.setName("누렁이");
+        dog4.setKind("진돗개");
+        dogService.insertDog(dog4);
+
+        Dog dog5 = new Dog();
+        dog5.setName("웰시 코르키");
+        dog5.setKind("웰시코기");
+        dogService.insertDog(dog5);
+
+        System.out.println(dogService.getAllDogs());
     }
 
     @Test
     public void testCountDogs(){
-        Dog dog = new Dog();
-        dog.setName("멍뭉이");
-        dog.setKind("포메리안");
-        dogService.insertDog(dog);
-
-        dog.setName("맥");
-        dog.setKind("말티즈");
-        dogService.insertDog(dog);
-
         List<Dog> dogs = dogService.getAllDogs();
-        System.out.println("result :: " + dogs);
+        System.out.println("CountDogs - all dogs :: " + dogs);
 
-        assertTrue(dogs.size() == 2);
+        assertTrue(dogs.size() == 5);
+        assertFalse(dogs.size() == 0);
     }
 
     @Test
     public void testGetDogsByName(){
-        Dog dog = new Dog();
-        dog.setName("멍뭉이");
-        dog.setKind("포메리안");
-        dogService.insertDog(dog);
-        List<Dog> dogs = dogService.getAllDogs();
-        System.out.println("result :: " + dogs);
+        String findDogName = "웰시 코르키";
 
-        Dog dog2 = new Dog();
-        dog2.setName("웰시 코르키");
-        dog2.setKind("웰시코기");
-        dogService.insertDog(dog2);
+        Dog foundDog = dogService.getDogsByName(findDogName);
 
-        dogs = dogService.getAllDogs();
-        System.out.println("result :: " + dogs);
-
-        Dog foundDog = dogService.getDogsByName(dog2.getName());
-
-        assertTrue(foundDog.getName() == dog2.getName());
-        assertEquals(true, foundDog.getName().equals(dog2.getName()));
+        assertTrue(foundDog.getName() == findDogName);
+        assertEquals(true, foundDog.getName().equals(findDogName));
+        assertThat(true, is(foundDog.getName().equals(findDogName)));
     }
-
 
     @Test
     public void testUpdateDogs() {
-        Dog dog = new Dog();
-        dog.setName("누렁이");
-        dog.setKind("진돗개");
-        dogService.insertDog(dog);
-
-        Dog foundDog = dogService.getDogsByName(dog.getName());
+        String findDogName = "누렁이";
+        Dog foundDog = dogService.getDogsByName(findDogName);
         assertTrue(foundDog.getKind().equals("진돗개"));
 
         Dog updateDog = new Dog();
-        updateDog.setName("누렁이");
+        //updateDog.setName("누렁이");
         updateDog.setKind("믹스견");
         dogService.updateDog(updateDog);
 
